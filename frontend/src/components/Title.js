7c79d0770786bb422343d8f1cdab9681c6ffa90f
230005 <230005eq@yse-c.net>
2024-06-24 12:33:00 +0900

    タイトルをDB上で読み取る

--- frontend/src/components/Title.js
+++ frontend/src/components/Title.js
@@ -1,14 +1,36 @@
 import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
 
 const Title = () => {
+  const [data, setData] = useState([]);
+
+  // 仮想的にデータを設定する useEffect
+  useEffect(() => {
+    // データベースから取得したデータを設定するイメージ
+    const fetchedData = [
+      { id: 1, name: 'おうどん'},
+      { id: 2, name: 'たべたい'},
+      { id: 3, name: 'ふ━━(　´_ゝ`)━(　´_ゝ`)━(　´_ゝ`)━━ん'},
+    ];
+    setData(fetchedData);
+  }, []);
   return (
-    <>
-      <h1>Simple_Threads</h1>
-      <div>
-        <p>メインジャンル</p>
-        <Link to={`/SubGenre/`}>サブジャンル</Link>
-      </div>
-    </>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>サブジャンル</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((Thread) => (
+            <tr key={Thread.id}>
+              <td><Link to={`/SubGenre/`}>{Thread.name}</Link></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
